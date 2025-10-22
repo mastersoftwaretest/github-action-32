37d115b8e84ab060dc7bc6e8af5db79d7e7d5090
mastersoftwaretest <tujiaqing.rachel@gmail.com>
2025-10-22 21:36:03 +0800

    Fix TFLint warnings

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
     key    = "jiaqing-s3-tf-ci.tfstate" #Change this
@@ -13,11 +22,10 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1] #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1] #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 }
-
